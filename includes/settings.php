792085e7dfe6cc374a68541e81c1935b226fd1bc
Niels <tnnielstn@gmail.com>
2016-02-19 00:03:43 +0100

    no message

--- includes/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = "localhost"; // DB HOST
-$dbuser = "root"; // DB USER
-$dbpass = ""; // DB PASSWORD
-$dbname = "tracklog"; // DB NAME
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-define("DB_PREFIX", ""); // DB PREFIX
-define("DEBUG", false);
