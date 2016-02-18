44504e390afa7c2a995b2cb832b7d15a07454a94
Ian Wensink <ian@burst-digital.com>
2016-02-18 14:10:10 +0100

    Stop tracking settings file

--- includes/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost = ""; // DB HOST
-$dbuser	= ""; // DB USER
-$dbpass = ""; // DB PASSWORD
-$dbname	= ""; // DB NAME
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-define("DB_PREFIX", ""); // DB PREFIX
