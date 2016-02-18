a1a08d6be355da566ab5d9803b7939fcc9540241
Ian Wensink <ian@burst-digital.com>
2016-02-18 14:05:43 +0100

    PHP base

---
+++ includes/settings.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = ""; // DB HOST
+$dbuser	= ""; // DB USER
+$dbpass = ""; // DB PASSWORD
+$dbname	= ""; // DB NAME
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+define("DB_PREFIX", ""); // DB PREFIX
