57bcecbbb8a7fc0d5ea6262133e3b41440d91ad7
Niels <tnnielstn@gmail.com>
2016-02-18 14:19:03 +0100

    Google Login

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.indigo-red.min.css">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
     <link rel="stylesheet" href="css/style.css">
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="953285646027-r3rsel8atqu2g8nbn45ag1jc24lah7lg.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -22,11 +25,13 @@
     </header>
     <main class="mdl-layout__content">
         <div class="page-content">
+            <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
         </div>
     </main>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="https://code.getmdl.io/1.1.1/material.min.js"></script>
 <script src="js/script.js"></script>
+<script src="js/googleLogin.js"></script>
 </body>
 </html>
