e2ff3d9fb5bc35268b1b61ed86fa4b09b0cf2428
Niels <tnnielstn@gmail.com>
2016-02-18 21:03:47 +0100

    Removed isLoggedIn

--- includes/functions.php
+++ includes/functions.php
@@ -1,10 +1,5 @@
 <?php
 	function isLoggedIn ()
 	{
-		return isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"] === true;
-	};
-
-	function checkAdmin ()
-	{
-		return isLoggedIn() && isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"] == true;
+		return isset($_SESSION["userId"]);
 	};
