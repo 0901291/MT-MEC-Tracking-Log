e29f747ee91e60c120b1f5cdf6c2c0405cbe23f6
Ian Wensink <ian@burst-digital.com>
2016-03-07 14:19:54 +0100

    Add pagination to API response

--- api/V1/api_entry.php
+++ api/V1/api_entry.php
@@ -4,22 +4,25 @@ require('objects/Entry.php');
 require('../../includes/objects/User.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
-$accept = $_SERVER['HTTP_ACCEPT'];
-$key = isset($_GET['api_key']) ? $_GET['api_key'] : null;
-$userId = User::getUserByToken($key, $db->getConnection());
+$accept = isset($_GET['accept']) ? 'application/'.$_GET['accept'] : $_SERVER['HTTP_ACCEPT'];
+$friendlyAccept = str_replace('application/', '', $accept);
+$apikey = isset($_GET['api_key']) ? $_GET['api_key'] : null;
+$userId = User::getUserByToken($apikey, $db->getConnection());
 $googleId = User::getGoogleIdByUserId($userId, $db->getConnection());
 $key = User::getKey($googleId);
 
 $input = json_decode(file_get_contents('php://input'));
 $post = $_POST;
 $values = null;
-$result = ["No method given."];
+$result = [];
 
 $entry = new Entry($db->getConnection());
 $user = new User($db->getConnection());
 
 if (isset($input)) $values = $input;
-    else if (sizeof($_POST) > 0) $values = $_POST;
+else if (sizeof($_POST) > 0) $values = $_POST;
+
+define('APIROOT', ROOT.'/api/v1');
 
 if ($userId != null) {
     $entry->id = (isset($_GET['id']) ? $_GET['id'] : null);
@@ -31,43 +34,78 @@ if ($userId != null) {
     $entry->companies = (isset($values['companies']) ? $values['companies'] : null);
     $entry->dataTypes = (isset($values['dataTypes']) ? $values['dataTypes'] : null);
     $entry->category = (isset($values['category']) ? htmlentities($values['category']) : null);
-    $date = (isset($values['date']) ? htmlentities($values['date']) : date("Y-m-d"));
-    $time = (isset($values['time']) ? htmlentities($values['time']) : date("H:i:s"));
-    $entry->date = date('Y-m-d H:i:s', strtotime("$date $time"));
+    $date = (isset($values['date']) ? htmlentities($values['date']) : date('Y-m-d'));
+    $time = (isset($values['time']) ? htmlentities($values['time']) : date('H:i:s'));
+    $entry->date = date('Y-m-d H:i:s', strtotime($date.' '.$time));
 
     switch ($method) {
         case 'GET':
             if ($entry->id != null) {
-                $result = $entry->detail($key, $userId);
+                $result['item'] = $entry->detail($key, $userId);
             } else {
-                $status = (isset($_GET['state']) ? $_GET['state'] : 0);
-                $result = Entry::getEntries($status, $db->getConnection(), $key, $userId);
+                $apiRoot = APIROOT.'/entry.'.$friendlyAccept.'?api_key='.$apikey;
+                $status = isset($_GET['state']) ? $_GET['state'] : 0;
+                $limit = isset($_GET['limit']) ? $_GET['limit'] : 0;
+                $offset = isset($_GET['offset']) ? $_GET['offset'] : 0;
+                $allItems = Entry::getEntries($status, $db->getConnection(), $key, $userId);
+                $total = count($allItems);
+                $result['items'] = array_slice($allItems, $offset, $limit);
+                $limit = $limit == 0 || $limit > $total ? $total : $limit;
+                $current = count($result['items']);
+                $totalPages = ceil($total / $current);
+                $currentPage = ceil(($offset + 1) / $limit);
+                $nextPageStart = ($offset + $limit) > $total ? $total : ($offset + $limit);
+                $nextPage = ceil(($offset + 1) / $limit) + 1 > ceil($total / $current) ? ceil($total / $current) : ceil(($offset + 1) / $limit) + 1;
+                $previousPageStart = $offset - $limit < 0 ? 0 : $offset - $limit;
+                $previousPage = ceil(($previousPageStart + 1) / $limit) < 1 ? 1 : ceil(($previousPageStart + 1)/ $limit);
+                if ($currentPage < 1) $currentPage = 1;
+                $result['pagination']['currentPage'] = $currentPage;
+                $result['pagination']['currentItems'] = $current;
+                $result['pagination']['totalPages'] = $totalPages;
+                $result['pagination']['totalItems'] = $total;
+                $nextPagination = [
+                    'rel' => 'next',
+                    'page' => $nextPage,
+                    'href' => $apiRoot.($limit < $total ? '&offset='.$nextPageStart.'&limit='.$limit : '')
+                ];
+                $previousPagination = [
+                    'rel' => 'previous',
+                    'page' => $previousPage,
+                    'href' => $apiRoot.($limit < $total ? '&offset='.$previousPageStart.'&limit='.$limit : '')
+                ];
+                if ($accept == 'application/json') {
+                    $result['links'] = [[
+                        'rel' => 'self',
+                        'href' => $apiRoot
+                    ]];
+                    $result['pagination']['links'] = [$previousPagination, $nextPagination];
+                }
             }
             break;
         case 'PUT':
-            $entry->state = ($entry->description == null ? 1 : 2);
+            $entry->state = $entry->description == null ? 1 : 2;
             $result = $entry->edit();
                 break;
         case 'DELETE':
             $result = $entry->delete();
             break;
         case 'POST':
-            $entry->state = ($entry->description == null ? 1 : 2);
+            $entry->state = $entry->description == null ? 1 : 2;
             $entry->insert();
             break;
     }
 } else {
     http_response_code(403);
-    $result = ['message' => 'Acces denied: invalid api_key'];
+    $result = ['message' => 'Access denied: invalid api_key'];
 }
 
 switch ($accept) {
     case 'application/json':
-        header("Content-type: application/json");
+        header('Content-type: application/json');
         print_r(json_encode($result));
         break;
     default:
-        header("Content-type: application/json");
+        header('Content-type: application/json');
         http_response_code(415);
-        print_r(json_encode(['message' => "We don't support this content-type."]));
+        print_r(json_encode(['message' => 'We don\'t support this content-type.']));
 }
