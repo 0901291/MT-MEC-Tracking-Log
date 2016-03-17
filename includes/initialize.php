0c77503575143a5d43efc1d873c164c5ba71c4c2
Niels <tnnielstn@gmail.com>
2016-03-17 23:14:19 +0100

    Added guzzle

--- includes/initialize.php
+++ includes/initialize.php
@@ -4,5 +4,6 @@ require('settings.php');
 require('functions.php');
 require('objects/Database.php');
 require('crypto/phpCrypt.php');
+require(__DIR__.'/../vendor/autoload.php');
 
 $db = new Database(DBHST, DBUSR, DBPASS, DBNAME);
